c18d60b71317016093214e3818f1df72f365a2e7
Alena Rehberger <alenarehberger92@gmail.com>
2021-11-23 16:14:55 -0600

    Trying to push build files to heroku

---
+++ client/build/precache-manifest.bc58740458c6203a9df7c585a11cb006.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5494ea35f40f1b6e24a33d2c1ee71ede",
+    "url": "/index.html"
+  },
+  {
+    "revision": "228d720bea7d9cb2b872",
+    "url": "/static/css/2.429e79c4.chunk.css"
+  },
+  {
+    "revision": "f6e236981f1867f52a2b",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "228d720bea7d9cb2b872",
+    "url": "/static/js/2.4b57a3cb.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.4b57a3cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6e236981f1867f52a2b",
+    "url": "/static/js/main.d4c22bef.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
