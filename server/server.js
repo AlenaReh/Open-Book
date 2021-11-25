2be1e2ab3f152ed1aee67090f99783c5b32579e3
Alena Rehberger <alenarehberger92@gmail.com>
2021-11-24 18:54:35 -0600

    Heroku depl

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const db = require("./config/connection");
 const path = require("path");
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
-
+require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
